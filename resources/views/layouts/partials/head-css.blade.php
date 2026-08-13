d8b634be27ddb2803565e0b47b1035bbee9419bc
Sciosdev <167479188+Sciosdev@users.noreply.github.com>
2026-08-13 03:42:49 -0600

    Corrige desbordamiento del resumen en móvil

--- resources/views/layouts/partials/head-css.blade.php
+++ resources/views/layouts/partials/head-css.blade.php
@@ -1,5 +1,5 @@
 @yield('css')
 @vite(['resources/scss/app.scss'])
 @vite(['resources/js/config.js'])
-<link rel="stylesheet" href="{{ asset('css/finance-mobile.css') }}?v=1">
+<link rel="stylesheet" href="{{ asset('css/finance-mobile.css') }}?v=2">
 @yield('css-after')
